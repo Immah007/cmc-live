f3c9bec8aa90369d2f9cd5a0dd696303de207afb
Immah007 <112688332+Immah007@users.noreply.github.com>
2026-07-28 04:25:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 
 // Optional: Handle controller route
 app.get('/controller', (req, res) => {
-    res.sendFile(path.join(__dirname, 'controller.html'));
+    res.sendFile(path.join(__dirname, 'public', 'controller.html'));
 });
 
 // ========== SOCKET.IO LOGIC ==========
