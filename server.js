5d010f5955437cda6e5faa841e95783979a145f1
Immah007 <112688332+Immah007@users.noreply.github.com>
2026-08-02 13:36:29 +0300

    Update controller route to serve from public directory

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/controller', (req, res) => {
-    res.sendFile(path.join(__dirname, 'controller.html'));
+    res.sendFile(path.join(__dirname, 'public', 'controller.html'));
 });
 
 // State management
